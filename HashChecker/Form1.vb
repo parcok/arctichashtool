39708a78c9ee501ecde757a6297ff180c7b32be5
parcok <parcok@sheridancollege.ca>
2017-02-12 01:12:44 -0500

    Added download links

--- HashChecker/Form1.vb
+++ HashChecker/Form1.vb
@@ -6,8 +6,23 @@ Imports System.Net
 Public Class Form1
 
     Dim Hashes = New String() {"F82EE297EA7487C6461F7C3E3DAF9FC7", "726ED4F381AE412605969EBC85A952A", "4436814B21BE96D1D22B39D74A4D2456", "6DC3168F0E26DF44D678EBF15292DF", "8A557CF0463940C63FE0577B95E5AF3", "C9F9A7B1AD490EAA9FA8B66C6E3E7EE", "BB9F98EDD2928A4C37F4470C0E12DA6", "4F63FBAFAE788E445C67735F2BEDC22A", "40970BD7AA7EF50771822E96D7CF4", "575E8FB6CA584AA24589E2391F805313", "CD164BE93EC0BDA2AA2DFF1DACF01817", "A256BF163F8A7950CDF96E62C6C1517", "DDE8115E1B666E26C9C365F98F9F4D", "A2215B6BC57DD62679BF72B07A93E6", "49209AD7A1B0D9D9793BDECF123F4B4A", "619D606A74EEEBE386D55545AEB92C4", "2C1DBF7C0EA2E5BEBA50C33FF1F147"}
-    Dim DownloadLinks = New String() {"https://dl.dropboxusercontent.com/content_link/8Tns3dfgt6cZ6cC0u6W0EtHLVjwX7yZOpYvOPJMYxCuJbx1jYwgABxSJ3cWeKsWH/file?dl=1"}
-    Dim DownloadURL = "http://arcticmseu.ddns.net/wz/"
+    Dim DownloadLinks = New String() {"http://www.mediafire.com/file/ewoid353twuignn/Base.wz",
+        "http://www.mediafire.com/file/abfki7v7fe8q93e/Character.wz",
+        "http://www.mediafire.com/file/4134xduecl3x465/Effect.wz",
+        "http://www.mediafire.com/file/g1y8p8ke4h71k3w/Etc.wz",
+        "http://www.mediafire.com/file/4j0txnqbk3wgr2m/Item.wz",
+        "http://www.mediafire.com/file/ye5feg5ebzve7xb/List.wz",
+        "http://www.mediafire.com/file/855xf79dzvjs4oo/Map.wz",
+        "http://www.mediafire.com/file/kjngug9mup3z6yi/Mob.wz",
+        "http://www.mediafire.com/file/910wdb46oddao2u/Morph.wz",
+        "http://www.mediafire.com/file/lqsllhy77g9ch0q/Npc.wz",
+        "http://www.mediafire.com/file/dl4byldoojy6ya1/Quest.wz",
+        "http://www.mediafire.com/file/qxub6kl79xc12x5/Reactor.wz",
+        "http://www.mediafire.com/file/uuut8yb76472mu3/Skill.wz",
+        "http://www.mediafire.com/file/rjr8rduu9f9hmlb/Sound.wz",
+        "http://www.mediafire.com/file/6evvzq5c4uo36f6/String.wz",
+        "http://www.mediafire.com/file/6goyfqtjucqs26c/TamingMob.wz",
+        "http://www.mediafire.com/file/8224o4mptgxt1ps/UI.wz"}
     Dim FileNames = New String() {"Base.wz", "Character.wz", "Effect.wz", "Etc.wz", "Item.wz", "List.wz", "Map.wz", "Mob.wz", "Morph.wz", "Npc.wz", "Quest.wz", "Reactor.wz", "Skill.wz", "Sound.wz", "String.wz", "TamingMob.wz", "UI.wz"}
     Dim UserHashes(16) As String
     Dim IncorrectHashes As New ArrayList()
