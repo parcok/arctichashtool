50e848d1ee1661e14bddf1a97da41de9a4960236
parcok <parcok@sheridancollege.ca>
2017-02-12 01:05:54 -0500

    Add project files.

---
+++ HashChecker/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Folder :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(66, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(263, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(335, 14)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Browse..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(335, 57)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 57)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(314, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(335, 124)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "test"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(424, 303)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "ArcticTools - Parco's Hash Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button3 As Button
+End Class
---
+++ HashChecker/Form1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Security.Cryptography
+Imports System.IO
+Imports System.Text
+Imports System.Net
+
+Public Class Form1
+
+    Dim Hashes = New String() {"F82EE297EA7487C6461F7C3E3DAF9FC7", "726ED4F381AE412605969EBC85A952A", "4436814B21BE96D1D22B39D74A4D2456", "6DC3168F0E26DF44D678EBF15292DF", "8A557CF0463940C63FE0577B95E5AF3", "C9F9A7B1AD490EAA9FA8B66C6E3E7EE", "BB9F98EDD2928A4C37F4470C0E12DA6", "4F63FBAFAE788E445C67735F2BEDC22A", "40970BD7AA7EF50771822E96D7CF4", "575E8FB6CA584AA24589E2391F805313", "CD164BE93EC0BDA2AA2DFF1DACF01817", "A256BF163F8A7950CDF96E62C6C1517", "DDE8115E1B666E26C9C365F98F9F4D", "A2215B6BC57DD62679BF72B07A93E6", "49209AD7A1B0D9D9793BDECF123F4B4A", "619D606A74EEEBE386D55545AEB92C4", "2C1DBF7C0EA2E5BEBA50C33FF1F147"}
+    Dim DownloadLinks = New String() {"https://dl.dropboxusercontent.com/content_link/8Tns3dfgt6cZ6cC0u6W0EtHLVjwX7yZOpYvOPJMYxCuJbx1jYwgABxSJ3cWeKsWH/file?dl=1"}
+    Dim DownloadURL = "http://arcticmseu.ddns.net/wz/"
+    Dim FileNames = New String() {"Base.wz", "Character.wz", "Effect.wz", "Etc.wz", "Item.wz", "List.wz", "Map.wz", "Mob.wz", "Morph.wz", "Npc.wz", "Quest.wz", "Reactor.wz", "Skill.wz", "Sound.wz", "String.wz", "TamingMob.wz", "UI.wz"}
+    Dim UserHashes(16) As String
+    Dim IncorrectHashes As New ArrayList()
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim fInfo As FileInfo()
+        Dim i As Integer = 0
+        Dim path As String = TextBox1.Text
+        Dim dInfo As New DirectoryInfo(path)
+
+        fInfo = dInfo.GetFiles("*.txt")
+        Dim files As String()
+        Dim FilePath As String
+        files = Directory.GetFiles(path, "*.wz")
+
+        If files.Count <> 17 Then
+            MsgBox("Missing or too many wz files, verify them and try again.")
+            Exit Sub
+        End If
+
+        Dim counter = 0
+        For Each FilePath In files
+            ' Get the file attributes and remove readonly
+            Dim attributes As FileAttributes
+            attributes = File.GetAttributes(FilePath)
+            If ((attributes And FileAttributes.ReadOnly) = FileAttributes.ReadOnly) Then
+                attributes = RemoveAttribute(attributes, FileAttributes.ReadOnly)
+                File.SetAttributes(FilePath, attributes)
+            End If
+            'Do Something With The File
+            Using RD As New FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+                'Dim RD As FileStream = New FileStream(File, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+                '   RD = New FileStream(File, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+                Dim md5 As MD5CryptoServiceProvider = New MD5CryptoServiceProvider
+                md5.ComputeHash(RD)
+                RD.Dispose()
+                RD.Close()
+
+                'converting the bytes into string
+                Dim hash As Byte() = md5.Hash
+                Dim SB As StringBuilder = New StringBuilder
+                Dim HB As Byte
+                For Each HB In hash
+                    SB.Append(String.Format("{0:X1}", HB))
+                Next
+                UserHashes(counter) = SB.ToString
+                counter = counter + 1
+            End Using
+            ProgressBar1.Value = 100 / files.Count * counter
+        Next
+
+        ' Comparing hashes 
+        counter = 0
+        For Each hash As String In UserHashes
+            If Hashes(counter) <> UserHashes(counter) Then
+                IncorrectHashes.Add(counter)
+            End If
+            counter = counter + 1
+        Next
+
+        If incorrectHashes.Count > 0 Then
+            Dim box As Integer = MessageBox.Show("You have corrupt/modified WZ files, would you like to download the clean versions?", "ArcticTools - Parco's Hash Checker", MessageBoxButtons.YesNo)
+            If box = DialogResult.Yes Then
+                ProgressBar1.Visible = True
+                DownloadFiles()
+                MsgBox("File downloads complete. If you continue to have issues contact staff for additional support.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
+            Else
+                MsgBox("You must fix your corrupt files in order to play ArcticMS. Please redownload or if you have used modified WZ files check they are working.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
+            End If
+        Else
+            MsgBox("All of your WZ files are perfect, contact staff for additional support.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
+        End If
+    End Sub
+
+    Private Sub DownloadFiles()
+        For Each number In IncorrectHashes
+            Dim link = DownloadURL & FileNames(number)
+            Dim path = TextBox1.Text & "\" & FileNames(number)
+            Dim client As WebClient = New WebClient
+            AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+            AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+            Console.WriteLine("Link:" & link)
+            Console.WriteLine("Path:" & path)
+            client.DownloadFile(New Uri(link), TextBox1.Text & "\" & FileNames(number))
+        Next
+    End Sub
+
+    Private Sub client_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+        Dim totalBytes As Double = Long.Parse(e.TotalBytesToReceive.ToString())
+        Dim percentage As Double = bytesIn / totalBytes * 100
+        Console.WriteLine(percentage & " " & totalBytes & " " & bytesIn)
+        ProgressBar1.Value = Int32.Parse(Math.Truncate(percentage).ToString())
+    End Sub
+
+    Private Sub client_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        MessageBox.Show("File downloaded.")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim link = "http://arcticmseu.ddns.net/wz/Character.wz"
+        Dim client As WebClient = New WebClient
+        Dim path = "C:\Users\Kevin\Desktop\Character.wz"
+        Dim attributes As FileAttributes
+        attributes = File.GetAttributes(path)
+        If ((attributes And FileAttributes.ReadOnly) = FileAttributes.ReadOnly) Then
+            attributes = RemoveAttribute(attributes, FileAttributes.ReadOnly)
+            File.SetAttributes(path, attributes)
+            Console.WriteLine("File is no longer read only.")
+        End If
+
+        client.DownloadFileAsync(New Uri(link), "C:\Users\Kevin\Desktop\Character.wz")
+    End Sub
+
+    Public Shared Function RemoveAttribute(ByVal attributes As FileAttributes, ByVal attributesToRemove As FileAttributes) As FileAttributes
+        Return attributes And (Not attributesToRemove)
+    End Function
+End Class
