a572cc81f1da2745b88ef8b348a3956d975635a0
parcok <parcok@sheridancollege.ca>
2017-02-13 15:47:49 -0500

    Working multiple, need to fix message and IOB

--- HashChecker/Form1.Designer.vb
+++ HashChecker/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
@@ -30,7 +30,6 @@ Partial Class Form1
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
         Me.SuspendLayout()
         '
         'Label1
@@ -83,14 +82,6 @@ Partial Class Form1
         Me.Button3.Text = "test"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'WebBrowser1
-        '
-        Me.WebBrowser1.Location = New System.Drawing.Point(15, 86)
-        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.WebBrowser1.Name = "WebBrowser1"
-        Me.WebBrowser1.Size = New System.Drawing.Size(594, 418)
-        Me.WebBrowser1.TabIndex = 7
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -102,7 +93,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.WebBrowser1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
@@ -119,5 +109,4 @@ Partial Class Form1
     Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
     Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents Button3 As Button
-    Friend WithEvents WebBrowser1 As WebBrowser
 End Class
--- HashChecker/Form1.vb
+++ HashChecker/Form1.vb
@@ -6,24 +6,9 @@ Imports System.Text.RegularExpressions
 
 Public Class Form1
 
-    Dim Hashes = New String() {"F82EE297EA7487C6461F7C3E3DAF9FC7", "726ED4F381AE412605969EBC85A952A", "4436814B21BE96D1D22B39D74A4D2456", "6DC3168F0E26DF44D678EBF15292DF", "8A557CF0463940C63FE0577B95E5AF3", "C9F9A7B1AD490EAA9FA8B66C6E3E7EE", "BB9F98EDD2928A4C37F4470C0E12DA6", "4F63FBAFAE788E445C67735F2BEDC22A", "40970BD7AA7EF50771822E96D7CF4", "575E8FB6CA584AA24589E2391F805313", "CD164BE93EC0BDA2AA2DFF1DACF01817", "A256BF163F8A7950CDF96E62C6C1517", "DDE8115E1B666E26C9C365F98F9F4D", "A2215B6BC57DD62679BF72B07A93E6", "49209AD7A1B0D9D9793BDECF123F4B4A", "619D606A74EEEBE386D55545AEB92C4", "2C1DBF7C0EA2E5BEBA50C33FF1F147"}
-    Dim DownloadLinks = New String() {"http://www.mediafire.com/file/ewoid353twuignn/Base.wz",
-        "http://www.mediafire.com/file/abfki7v7fe8q93e/Character.wz",
-        "http://www.mediafire.com/file/4134xduecl3x465/Effect.wz",
-        "http://www.mediafire.com/file/g1y8p8ke4h71k3w/Etc.wz",
-        "http://www.mediafire.com/file/4j0txnqbk3wgr2m/Item.wz",
-        "http://www.mediafire.com/file/ye5feg5ebzve7xb/List.wz",
-        "http://www.mediafire.com/file/855xf79dzvjs4oo/Map.wz",
-        "http://www.mediafire.com/file/kjngug9mup3z6yi/Mob.wz",
-        "http://www.mediafire.com/file/910wdb46oddao2u/Morph.wz",
-        "http://www.mediafire.com/file/lqsllhy77g9ch0q/Npc.wz",
-        "http://www.mediafire.com/file/dl4byldoojy6ya1/Quest.wz",
-        "http://www.mediafire.com/file/qxub6kl79xc12x5/Reactor.wz",
-        "http://www.mediafire.com/file/uuut8yb76472mu3/Skill.wz",
-        "http://www.mediafire.com/file/rjr8rduu9f9hmlb/Sound.wz",
-        "http://www.mediafire.com/file/6evvzq5c4uo36f6/String.wz",
-        "http://www.mediafire.com/file/6goyfqtjucqs26c/TamingMob.wz",
-        "http://www.mediafire.com/file/8224o4mptgxt1ps/UI.wz"}
+    Dim client As WebClient = New WebClient
+    Dim Hashes = New String() {"F72EE297EA7487C6461F7C3E3DAF9FC7", "726ED4F381AE412605969EBC85A952A", "4436814B21BE96D1D22B39D74A4D2456", "6DC3168F0E26DF44D678EBF15292DF", "8A557CF0463940C63FE0577B95E5AF3", "C9F9A7B1AD490EAA9FA8B66C6E3E7EE", "BB9F98EDD2928A4C37F4470C0E12DA6", "4F63FBAFAE788E445C67735F2BEDC22A", "40970BD7AA7EF50771822E96D7CF4", "575E8FB6CA584AA24589E2391F805313", "CD164BE93EC0BDA2AA2DFF1DACF01817", "A256BF163F8A7950CDF96E62C6C1517", "DDE8115E1B666E26C9C365F98F9F4D", "A2215B6BC57DD62679BF72B07A93E6", "49209AD7A1B0D9D9793BDECF123F4B4A", "619D606A74EEEBE386D55545AEB92C4", "2C1DBF7C0EA2E5BEBA50C33FF1F147"}
+    Dim fileHost = "http://arcticmseu.ddns.net/wz/"
     Dim FileNames = New String() {"Base.wz", "Character.wz", "Effect.wz", "Etc.wz", "Item.wz", "List.wz", "Map.wz", "Mob.wz", "Morph.wz", "Npc.wz", "Quest.wz", "Reactor.wz", "Skill.wz", "Sound.wz", "String.wz", "TamingMob.wz", "UI.wz"}
     Dim UserHashes(16) As String
     Dim IncorrectHashes As New ArrayList()
@@ -47,10 +32,11 @@ Public Class Form1
         files = Directory.GetFiles(path, "*.wz")
 
         If files.Count <> 17 Then
-            MsgBox("Missing or too many wz files, verify them and try again.")
+            MsgBox("Missing or too many wz files, verify them and try again.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
             Exit Sub
         End If
 
+        MsgBox("Checking Wz files, this may take a minute, let the program run.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
         Dim counter = 0
         For Each FilePath In files
             ' Get the file attributes and remove readonly
@@ -91,12 +77,13 @@ Public Class Form1
             counter = counter + 1
         Next
 
+        For Each fileName In IncorrectHashes
+            Console.WriteLine(fileName)
+        Next
         If IncorrectHashes.Count > 0 Then
             Dim box As Integer = MessageBox.Show("You have corrupt/modified WZ files, would you like to download the clean versions?", "ArcticTools - Parco's Hash Checker", MessageBoxButtons.YesNo)
             If box = DialogResult.Yes Then
-                ProgressBar1.Visible = True
                 DownloadFiles()
-                MsgBox("File downloads complete. If you continue to have issues contact staff for additional support.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
             Else
                 MsgBox("You must fix your corrupt files in order to play ArcticMS. Please redownload or if you have used modified WZ files check they are working.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
             End If
@@ -106,44 +93,50 @@ Public Class Form1
     End Sub
 
     Private Sub DownloadFiles()
-        For Each number In IncorrectHashes
-            Dim link = isolateMediafireLink(DownloadLinks(number))
-            Dim path = TextBox1.Text & "\" & FileNames(number)
-            Dim client As WebClient = New WebClient
-            AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+        'For Each number In IncorrectHashes
+        Dim link = fileHost & FileNames(IncorrectHashes(0))
+        Console.WriteLine(link)
+        Dim path = TextBox1.Text & "\" & FileNames(IncorrectHashes(0))
+        AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
             AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
             Console.WriteLine("Link:" & link)
             Console.WriteLine("Path:" & path)
-            client.DownloadFile(New Uri(link), TextBox1.Text & "\" & FileNames(number))
-        Next
+        client.DownloadFileAsync(New Uri(link), TextBox1.Text & "\" & FileNames(IncorrectHashes(0)))
+        'Next
+        MsgBox("File downloads complete. If you continue to have issues contact staff for additional support.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
     End Sub
 
     Private Sub client_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
         Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
         Dim totalBytes As Double = Long.Parse(e.TotalBytesToReceive.ToString())
         Dim percentage As Double = bytesIn / totalBytes * 100
-        Console.WriteLine(percentage & " " & totalBytes & " " & bytesIn)
+        'Console.WriteLine("Percentage: " & percentage & " Total Bytes: " & totalBytes & " Bytes In: " & bytesIn)
         ProgressBar1.Value = Int32.Parse(Math.Truncate(percentage).ToString())
     End Sub
 
     Private Sub client_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
-        MessageBox.Show("File downloaded.")
+        MsgBox("File downloaded.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
+        If IncorrectHashes.Count > 0 Then
+            IncorrectHashes.RemoveAt(0)
+            DownloadFiles()
+        End If
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        'Dim link = "http://arcticmseu.ddns.net/wz/Character.wz"
-        'Dim client As WebClient = New WebClient
-        'Dim path = "C:\Users\Kevin\Desktop\Character.wz"
-        'Dim attributes As FileAttributes
-        'attributes = File.GetAttributes(path)
-        'If ((attributes And FileAttributes.ReadOnly) = FileAttributes.ReadOnly) Then
-        ' attributes = RemoveAttribute(attributes, FileAttributes.ReadOnly)
-        ' File.SetAttributes(path, attributes)
-        ' Console.WriteLine("File is no longer read only.")
-        ' End If
+        Dim link = "http://arcticmseu.ddns.net/wz/Etc.wz"
+        Dim client As WebClient = New WebClient
+        Dim path = "C:\Users\Kevin\Desktop\Etc.wz"
+        Dim attributes As FileAttributes
+        attributes = File.GetAttributes(path)
+        If ((attributes And FileAttributes.ReadOnly) = FileAttributes.ReadOnly) Then
+            attributes = RemoveAttribute(attributes, FileAttributes.ReadOnly)
+            File.SetAttributes(path, attributes)
+        End If
 
-        '        client.DownloadFileAsync(New Uri(link), "C:\Users\Kevin\Desktop\Character.wz")
-        isolateMediafireLink("http://www.mediafire.com/file/abfki7v7fe8q93e/Character.wz")
+        AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+        AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+        client.DownloadFileAsync(New Uri(link), "C:\Users\Kevin\Desktop\Character.wz")
+        'isolateMediafireLink("http://www.mediafire.com/file/abfki7v7fe8q93e/Character.wz")
     End Sub
 
     Public Shared Function RemoveAttribute(ByVal attributes As FileAttributes, ByVal attributesToRemove As FileAttributes) As FileAttributes
@@ -151,34 +144,22 @@ Public Class Form1
     End Function
 
     Public Shared Function isolateMediafireLink(mediafireLink As String)
-        'Dim webc As WebClient = New WebClient
-        'webc.Headers("User-Agent") = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-        'webc.Headers("Referer") = "http://www.mediafire.com/file/abfki7v7fe8q93e/Character.wz"
-        'webc.Headers("Accept-Language") = "en-US,en;q=0.8"
-        'webc.Headers("Accept") = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-        'webc.Headers("Cookie") = "FastPopSessionRequestNumber=3;"
-        'webc.Headers("Accept-Encoding") = "gzip, deflate, sdch"
-        'webc.Headers("Upgrade-Insecure-Requests") = "1"
-        'webc.Headers("Cache-Control") = "max-age=0"
-        'Dim webSource As String = webc.DownloadString(mediafireLink)
+        Dim webc As WebClient = New WebClient
+        webc.Headers("User-Agent") = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+        webc.Headers("Accept-Language") = "en-US,en;q=0.8"
+        webc.Headers("Accept") = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+        Dim webSource As String = webc.DownloadString(mediafireLink)
         'Console.WriteLine(webSource)
-        Form1.WebBrowser1.Navigate(mediafireLink)
-        While Form1.WebBrowser1.ReadyState <> WebBrowserReadyState.Complete
-            Application.DoEvents()
-        End While
-        MessageBox.Show("Loaded")
-
-        Dim webSource As String = Form1.WebBrowser1.DocumentText
         Dim regex = New Regex("http:\/\/download\d{4}\.mediafire.com.+?'")
         Dim match As Match = regex.Match(webSource)
         Dim directDownload As String
         If match.Success Then
             directDownload = match.Groups(1).Value
             Console.WriteLine("Link: " & directDownload)
-            MsgBox(directDownload)
+            MsgBox(directDownload, MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
         Else
             Console.WriteLine("No value found.")
-            MsgBox("No value found.")
+            MsgBox("No value found.", MessageBoxButtons.OK, "ArcticTools - Parco's Hash Checker")
         End If
         Return "Hello"
     End Function
